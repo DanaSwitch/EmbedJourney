0040e19fd3404f35a5b071569c42b2a81b932cac
S_zaiwu <S_zaiwu@outlook.com>
2026-01-13 11:44:08 +0800

    Upgrade file structure.

--- Resource/STM32入门教程资料/程序源码/STM32Project-无注释版/3-2 LED流水灯/User/main.c
+++ Resource/STM32入门教程资料/程序源码/STM32Project-无注释版/3-2 LED流水灯/User/main.c
@@ -14,20 +14,20 @@ int main(void)
 	while (1)
 	{
 		GPIO_Write(GPIOA, ~0x0001);	//0000 0000 0000 0001
-		Delay_ms(100);
+		Delay_ms(500);
 		GPIO_Write(GPIOA, ~0x0002);	//0000 0000 0000 0010
-		Delay_ms(100);
+		Delay_ms(500);
 		GPIO_Write(GPIOA, ~0x0004);	//0000 0000 0000 0100
-		Delay_ms(100);
+		Delay_ms(500);
 		GPIO_Write(GPIOA, ~0x0008);	//0000 0000 0000 1000
-		Delay_ms(100);
+		Delay_ms(500);
 		GPIO_Write(GPIOA, ~0x0010);	//0000 0000 0001 0000
-		Delay_ms(100);
+		Delay_ms(500);
 		GPIO_Write(GPIOA, ~0x0020);	//0000 0000 0010 0000
-		Delay_ms(100);
+		Delay_ms(500);
 		GPIO_Write(GPIOA, ~0x0040);	//0000 0000 0100 0000
-		Delay_ms(100);
+		Delay_ms(500);
 		GPIO_Write(GPIOA, ~0x0080);	//0000 0000 1000 0000
-		Delay_ms(100);
+		Delay_ms(500);
 	}
 }
